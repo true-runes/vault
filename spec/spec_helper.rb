e6525d89233cc1759f05f24b09079c68ac4ef23d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-06-11 22:21:23 +0900

    Add rspec configuration files

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+# こちらはデフォルトのままにしておき、設定は全て rails_helper.rb に書く
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
