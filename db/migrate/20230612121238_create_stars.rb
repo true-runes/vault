7f53b7962d1b63c2d2203d33ab2e7c6042c2140d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-06-12 21:16:48 +0900

    Add star table with name, name_en, seating_order

--- db/migrate/20230612121238_create_stars.rb
+++ db/migrate/20230612121238_create_stars.rb
@@ -1,8 +1,15 @@
 class CreateStars < ActiveRecord::Migration[7.0]
   def change
     create_table :stars do |t|
+      t.string :name, null: false, comment: '宿星の名前（日本語）'
+      t.string :name_en, null: false, comment: '宿星の名前（英語）'
+      t.integer :seating_order, null: false, comment: '席次'
 
       t.timestamps
     end
+
+    add_index :stars, :name, unique: true
+    add_index :stars, :name_en, unique: true
+    add_index :stars, :seating_order, unique: true
   end
 end
