2ba8454e97b419773c6481ed2485678f7d520b43
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-03 18:25:03 +0900

    ci: 🎡 CI でインポートを前提としたテストを行えるようにした (#56)
    * ci: 🎡 CI でインポートを前提としたテストを行えるようにした
    
    * ci: 🎡 RAILS_ENV=test を明記した

--- spec/models/title_spec.rb
+++ spec/models/title_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Title, type: :model do
   describe 'Title クラス' do
-    it 'クラス名が class であること' do
-      expect(Title).to be_a Class
+    it 'タイトルの数が 11 であること' do
+      expect(Title.count).to eq 11
     end
   end
 end
