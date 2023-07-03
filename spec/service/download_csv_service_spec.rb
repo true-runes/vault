bc3196cf33886b582f30195fe4760fc8c5329f8a
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-03 18:01:47 +0900

    ci: 🎡 一部のテストは CI 上では行わないようにした（設定が煩雑なため）

--- spec/service/download_csv_service_spec.rb
+++ spec/service/download_csv_service_spec.rb
@@ -8,27 +8,30 @@ describe DownloadCsvService do
     )
   end
 
+  # 設定ファイルの煩雑さ上、CI ではスキップする
   describe 'CSVファイル' do
-    it '期待どおりにダウンロード（＝生成）されていること' do
-      service.execute('tmp/vault_test.csv')
+    unless ENV['CI'] == 'true'
+      it '期待どおりにダウンロード（＝生成）されていること' do
+        service.execute('tmp/vault_test.csv')
 
-      expect(File.exist?('tmp/vault_test.csv')).to be_truthy
-    end
+        expect(File.exist?('tmp/vault_test.csv')).to be_truthy
+      end
 
-    it '一行目（ヘッダ）の内容が期待どおりであること' do
-      service.execute('tmp/vault_test_check_first_row.csv')
+      it '一行目（ヘッダ）の内容が期待どおりであること' do
+        service.execute('tmp/vault_test_check_first_row.csv')
 
-      expect(
-        File.open('tmp/vault_test_check_first_row.csv').readline.chomp
-      ).to eq 'id,seating_order,name,name_en'
-    end
+        expect(
+          File.open('tmp/vault_test_check_first_row.csv').readline.chomp
+        ).to eq 'id,seating_order,name,name_en'
+      end
 
-    it '二行目（内容の行の一行目）の内容が期待どおりであること' do
-      service.execute('tmp/vault_test_check_second_row.csv')
+      it '二行目（内容の行の一行目）の内容が期待どおりであること' do
+        service.execute('tmp/vault_test_check_second_row.csv')
 
-      expect(
-        File.open('tmp/vault_test_check_first_row.csv').readlines[1].chomp
-      ).to eq '1,1,天魁星,Leader Star'
+        expect(
+          File.open('tmp/vault_test_check_first_row.csv').readlines[1].chomp
+        ).to eq '1,1,天魁星,Leader Star'
+      end
     end
   end
 end
