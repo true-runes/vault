62ba4e84666cb5c22d6ef181368664b835d1ff21
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-04 17:04:19 +0900

    デプロイリリースタスクの証明をするためのファイルを作成する

--- heroku_release_script.sh
+++ heroku_release_script.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
-/usr/bin/touch /tmp/this_is_a_prove_that_deploy_release_task_by_procfile
+/usr/bin/echo "Hello, World!" > /tmp/this_is_a_prove_that_deploy_release_task_by_procfile
 
-AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} aws s3 cp /tmp/this_is_a_prove_that_deploy_release_task_by_procfile s3://${AWS_BUCKET_NAME} --region ap-northeast-1
+cd tmp || exit
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+cd aws/dist || exit
+
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} ./aws s3 cp /tmp/this_is_a_prove_that_deploy_release_task_by_procfile s3://${AWS_BUCKET_NAME} --region ap-northeast-1
+
+exit 0
