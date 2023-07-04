aacf93f9df965544dc7398c76577cc9428783330
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-04 13:13:52 +0900

    feat: 🎸 Procfile を導入した

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}
+release: touch /tmp/this_is_a_prove_that_deploy_release_task_by_procfile
---
+++ config/initializers/constant_value.rb
@@ -0,0 +1,28 @@
+module Vault
+  class ConstantValue
+    class << self
+      def class_name_to_csv_filepath_map
+        {
+          'ImportService::OnSheet::Star' => Rails.root.join(
+            Rails.root.join('db/csv_on_sheet/basic_attributes/stars.csv')
+          ),
+          'ImportService::OnSheet::Title' => Rails.root.join(
+            Rails.root.join('db/csv_on_sheet/products/titles.csv')
+          ),
+          'ImportService::OnSheet::Platform' => Rails.root.join(
+            Rails.root.join('db/csv_on_sheet/basic_attributes/platforms.csv')
+          ),
+          'ImportService::OnSheet::GssSectionHqCharacter' => Rails.root.join(
+            Rails.root.join('db/csv_on_sheet/gss_hq_characters_section/web_format_data.csv')
+          ),
+          'ImportService::OnSheet::GssSectionCity' => Rails.root.join(
+            Rails.root.join('db/csv_on_sheet/gss_cities_section/web_format_data.csv')
+          ),
+          'ImportService::OnSheet::GssCharacter' => Rails.root.join(
+            Rails.root.join('db/csv_on_sheet/gss_character/web_format_data.csv')
+          )
+        }
+      end
+    end
+  end
+end
