b65232b4b3ed9292528eea34af32190cfad3b877
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-04 16:57:10 +0900

    Procfileを更新し、一部の不要なコメントを削除しました。デプロイ時にリリースタスクを実行するスクリプトも更新しました。

--- heroku_release_script.sh
+++ heroku_release_script.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 
 /usr/bin/touch /tmp/this_is_a_prove_that_deploy_release_task_by_procfile
+
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} aws s3 cp /tmp/this_is_a_prove_that_deploy_release_task_by_procfile s3://${AWS_BUCKET_NAME} --region ap-northeast-1
