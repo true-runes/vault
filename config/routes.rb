0edb966200c87c02e0454abc523930fa67d84400
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-17 09:15:04 +0900

    feat: 🎸 /api/v1/characters エンドポイントを作成した (#89)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :stars, only: [:index]
       resources :nicknames, only: %i[index show], param: :name
+
+      namespace :gss do
+        resources :characters, only: %i[index show], param: :name
+      end
     end
   end
 end
