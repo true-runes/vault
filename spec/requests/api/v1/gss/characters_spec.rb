ecc510099f56a982cb740f93fe7c5b5421749abf
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-24 12:33:14 +0900

    feat: 🎸 幻水V が product_title から抜け落ちていたので修正した

--- spec/requests/api/v1/gss/characters_spec.rb
+++ spec/requests/api/v1/gss/characters_spec.rb
@@ -28,14 +28,37 @@ RSpec.describe 'Api::V1::Gss::Characters', type: :request do
       it { expect(response).to have_http_status(:success) }
     end
 
-    describe '戻り値の key' do
+    describe '戻り値（フリック）' do
       before do
         get '/api/v1/gss/characters/%E3%83%95%E3%83%AA%E3%83%83%E3%82%AF'
       end
 
-      it '期待どおりの値を含むこと' do
+      it 'keys が期待どおりの値を含むこと' do
         expect(JSON.parse(response.body).keys).to eq(
           %w[gss_name nicknames product_titles]
+        )
+      end
+    end
+
+    describe '戻り値（ゲオルグ）' do
+      before do
+        get '/api/v1/gss/characters/%E3%82%B2%E3%82%AA%E3%83%AB%E3%82%B0'
+      end
+
+      it 'keys に期待どおりの値を含むこと' do
+        expect(JSON.parse(response.body).keys).to be_empty
+      end
+    end
+
+    describe '戻り値（ゲオルグ・プライム）' do
+      before do
+        get '/api/v1/gss/characters/%E3%82%B2%E3%82%AA%E3%83%AB%E3%82%B0%E3%83%BB%E3%83%97%E3%83%A9%E3%82%A4%E3%83%A0'
+      end
+
+      it 'product_titles に期待どおりの値を含むこと' do
+        # 順番は保証されないでよい
+        expect(JSON.parse(response.body)['product_titles']).to match_array(
+          %w[幻想水滸伝II 幻想水滸外伝Vol.1 幻想水滸伝V]
         )
       end
     end
