a54c7be4fb3c673848d089be33070ecbb96eccc5
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-24 12:52:18 +0900

    feat: 🎸 ゲオルグ -> ゲオルグ・プライム のシート状の修正とそれに伴う CSVアップデート および テストの追加 (#99)

--- scripts/exec_local_init_rspec.sh
+++ scripts/exec_local_init_rspec.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# test 環境で作る必要がある
+# - DB の構築不備の際に一回実行する
+# - test 環境で作る必要がある
+# - bundle exec rails db:download_csv は実行されないので注意
 RAILS_ENV="test" bundle exec rails db:import_csv
 RAILS_ENV="test" bundle exec rails db:make_up_db
 RAILS_ENV="test" bundle exec rspec
