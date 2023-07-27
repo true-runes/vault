58f94e51559755118cfd2adbfecbd905f184d6b0
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-27 22:14:31 +0900

    feat: 🎸 Constant で Gss キャラ名を取得するクラスを更新した

--- all_scripts_execution.sh
+++ all_scripts_execution.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 scripts/csv_downloader.sh
-scripts/db_construction.sh
-scripts/exec_local_rspec.sh
+scripts/import_csv_and_make_up_db.sh
+scripts/exec_local_init_rspec.sh
 
 exit 0
