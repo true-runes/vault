74b06036d85a18b598137847054487ce44065fca
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-28 07:49:42 +0900

    feat: 🎸 どの登場作品にも属さないキャラクターのメソッドを追加した（ヒクサク想定）

--- app/lib/constant/gss_character_names.rb
+++ app/lib/constant/gss_character_names.rb
@@ -40,6 +40,12 @@ module Constant
         ProductTitle.find_by(name: '幻想水滸伝 紡がれし百年の時').gss_characters.pluck(:name).sort
       end
 
+      # いずれの登場作品にも属さないキャラクター
+      def others
+        # 実質は LEFT OUTER JOIN で WHERE "product_titles"."id" IS NULL をしている
+        Gss::Character.where.missing(:product_titles).pluck(:name).sort
+      end
+
       def values
         (s1 + s2 + gaiden + s3 + s4 + rhapsodia + s5 + tk + tsumutoki).uniq.sort
       end
