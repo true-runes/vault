100c26fd4230eda2f33a7350676a06eacf556894
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-28 05:27:50 +0900

    feat: 🎸 Constant で取得するキャラ名に sort を付けた

--- app/lib/constant/gss_character_names.rb
+++ app/lib/constant/gss_character_names.rb
@@ -3,40 +3,42 @@ module Constant
   class GssCharacterNames
     class << self
       def s1
-        ProductTitle.find_by(name: '幻想水滸伝').gss_characters.pluck(:name)
+        ProductTitle.find_by(name: '幻想水滸伝').gss_characters.pluck(:name).sort
       end
 
       def s2
-        ProductTitle.find_by(name: '幻想水滸伝II').gss_characters.pluck(:name)
+        ProductTitle.find_by(name: '幻想水滸伝II').gss_characters.pluck(:name).sort
       end
 
       def gaiden
-        ProductTitle.find_by(name: '幻想水滸外伝Vol.1').gss_characters.pluck(:name) &&
+        (
+          ProductTitle.find_by(name: '幻想水滸外伝Vol.1').gss_characters.pluck(:name) &&
           ProductTitle.find_by(name: '幻想水滸外伝Vol.2').gss_characters.pluck(:name)
+        ).sort
       end
 
       def s3
-        ProductTitle.find_by(name: '幻想水滸伝III').gss_characters.pluck(:name)
+        ProductTitle.find_by(name: '幻想水滸伝III').gss_characters.pluck(:name).sort
       end
 
       def s4
-        ProductTitle.find_by(name: '幻想水滸伝IV').gss_characters.pluck(:name)
+        ProductTitle.find_by(name: '幻想水滸伝IV').gss_characters.pluck(:name).sort
       end
 
       def rhapsodia
-        ProductTitle.find_by(name: 'Rhapsodia').gss_characters.pluck(:name)
+        ProductTitle.find_by(name: 'Rhapsodia').gss_characters.pluck(:name).sort
       end
 
       def s5
-        ProductTitle.find_by(name: '幻想水滸伝V').gss_characters.pluck(:name)
+        ProductTitle.find_by(name: '幻想水滸伝V').gss_characters.pluck(:name).sort
       end
 
       def tk
-        ProductTitle.find_by(name: '幻想水滸伝ティアクライス').gss_characters.pluck(:name)
+        ProductTitle.find_by(name: '幻想水滸伝ティアクライス').gss_characters.pluck(:name).sort
       end
 
       def tsumutoki
-        ProductTitle.find_by(name: '幻想水滸伝 紡がれし百年の時').gss_characters.pluck(:name)
+        ProductTitle.find_by(name: '幻想水滸伝 紡がれし百年の時').gss_characters.pluck(:name).sort
       end
 
       def values
