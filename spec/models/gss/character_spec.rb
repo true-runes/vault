a4fcc46b1c3470c4e0a12e2d566114fc57def483
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-28 08:14:10 +0900

    test: 💍 Spec を追加した (#109)

--- spec/models/gss/character_spec.rb
+++ spec/models/gss/character_spec.rb
@@ -24,4 +24,28 @@ RSpec.describe Gss::Character, type: :model do
       expect(Gss::Character.where(name: 'ナッシュ・ラトキエ（ナッシュ・クロービス）')).to be_present
     end
   end
+
+  describe 'キャラが作品に属する' do
+    it 'ルック が 幻水I, II and III に属すること' do
+      character = Gss::Character.find_by(name: 'ルック')
+
+      expect(character.product_titles.map(&:name)).to match_array %w[
+        幻想水滸伝
+        幻想水滸伝II
+        幻想水滸伝III
+      ]
+    end
+
+    it 'ハーン・カニンガム が 幻水II に属すること' do
+      character = Gss::Character.find_by(name: 'ハーン・カニンガム')
+
+      expect(character.product_titles.map(&:name)).to match_array %w[幻想水滸伝II]
+    end
+
+    it 'コノン が 幻想水滸伝ティアクライス に属すること' do
+      character = Gss::Character.find_by(name: 'コノン')
+
+      expect(character.product_titles.map(&:name)).to match_array %w[幻想水滸伝ティアクライス]
+    end
+  end
 end
