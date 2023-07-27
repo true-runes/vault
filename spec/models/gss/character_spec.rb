c105ffc8a91d0a8701216e519cd4b8ef86f74337
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-07-28 06:53:02 +0900

    fix: 🐛 アーサーを IV の出現キャラから除外し、ジョウイ、ザジ、ワンを修正 (#105)
    * fix: 🐛 アーサーを IV の出現キャラから除外した
    
    * feat: 🎸 ジョウイ、ザジ、ワンを修正
    
    * feat: 🎸 nicknames の CSV も更新した
    
    * fix: 🐛 ナッシュ・ラトキエ（ナッシュ・クロービス） が不適切だったので修正

--- spec/models/gss/character_spec.rb
+++ spec/models/gss/character_spec.rb
@@ -6,4 +6,22 @@ RSpec.describe Gss::Character, type: :model do
       expect(Gss::Character.all.count).to eq Gss::Character.all.distinct.count
     end
   end
+
+  describe 'キャラ' do
+    it '"ジョーカー" という名前が含まれるキャラは 1人 だけであること' do
+      expect(Gss::Character.where('name like ?', '%ジョーカー%').count).to eq 1
+    end
+
+    it '"ジョーカー（ワン）" という名前のキャラが存在すること' do
+      expect(Gss::Character.where(name: 'ジョーカー（ワン）')).to be_present
+    end
+
+    it '"ザジ・キュイロス（サナトス・クロフォード）" という名前のキャラが存在すること' do
+      expect(Gss::Character.where(name: 'ザジ・キュイロス（サナトス・クロフォード）')).to be_present
+    end
+
+    it '"ナッシュ・ラトキエ（ナッシュ・クロービス）" という名前のキャラが存在すること' do
+      expect(Gss::Character.where(name: 'ナッシュ・ラトキエ（ナッシュ・クロービス）')).to be_present
+    end
+  end
 end
